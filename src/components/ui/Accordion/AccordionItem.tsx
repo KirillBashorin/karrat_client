5202a5d8c51c451cf89d5a375fbb0636bdd885ef
KirllBashorin <99118225+KirllBashorin@users.noreply.github.com>
2024-10-11 16:31:05 +0400

    Faq fixes

--- src/components/ui/Accordion/AccordionItem.tsx
+++ src/components/ui/Accordion/AccordionItem.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { FC } from 'react';
+import React, { FC, useEffect, useRef } from 'react';
 import clsx from 'clsx';
 
 import { Title } from '@/components/ui';
@@ -17,15 +17,47 @@ interface AccordionItemProps {
 }
 
 const AccordionItem: FC<AccordionItemProps> = ({ isOpened, onClick, title, text }) => {
+  const itemRef = useRef<HTMLLIElement>(null);
+  const wrapperRef = useRef<HTMLDivElement>(null);
+
+  const handlerClick = () => {
+    onClick();
+  };
+
+  useEffect(() => {
+    const wrapper = wrapperRef.current;
+
+    const handleTransitionEnd = () => {
+      if (!isOpened || !itemRef.current) return;
+
+      const itemTop = itemRef.current.getBoundingClientRect().top;
+      const viewportHeight = window.innerHeight;
+
+      if (itemTop < 0 || itemTop > viewportHeight) {
+        itemRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      }
+    };
+
+    if (!wrapper) return;
+
+    wrapper.addEventListener('transitionend', handleTransitionEnd);
+
+    return () => {
+      if (!wrapper) return;
+
+      wrapper.removeEventListener('transitionend', handleTransitionEnd);
+    };
+  }, [isOpened]);
+
   return (
-    <li className={clsx(isOpened && styles.opened)}>
-      <button className={styles.itemButton} type='button' onClick={onClick}>
+    <li className={clsx(isOpened && styles.opened)} ref={itemRef}>
+      <button className={styles.itemButton} type='button' onClick={handlerClick}>
         <Title className={styles.itemTitle} size={'small'} as={'h3'}>
           {title}
         </Title>
         <TickIcon width={24} height={24} />
       </button>
-      <div className={styles.itemWrapper}>
+      <div className={styles.itemWrapper} ref={wrapperRef}>
         <div className={styles.itemContent}>
           <div className={styles.itemText}>{text}</div>
         </div>
