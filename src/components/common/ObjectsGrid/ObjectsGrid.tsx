df05d92c7cb805481e5725567c38efd9efb57af2
KirllBashorin <99118225+KirllBashorin@users.noreply.github.com>
2024-10-18 18:31:00 +0400

    MarketObjectTabs

--- src/components/common/ObjectsGrid/ObjectsGrid.tsx
+++ src/components/common/ObjectsGrid/ObjectsGrid.tsx
@@ -1,13 +1,17 @@
 'use client';
 
-import React, { FC } from 'react';
+import React, { FC, useState, useRef } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Grid } from 'swiper/modules';
+import { Grid, Pagination, Navigation } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/grid';
+import 'swiper/css/pagination';
+
+import ArrowCircleIcon from '/public/images/icons/arrow-circle.svg';
 
 import { ObjectPreview } from '@/components/common';
 import { ObjectType } from '@/types';
+import { Button } from '@/components/ui';
 
 import styles from './ObjectsGrid.module.scss';
 
@@ -16,32 +20,71 @@ interface ObjectsGridProps {
 }
 
 const ObjectsGrid: FC<ObjectsGridProps> = ({ objects }) => {
+  const swiperRef = useRef<any>(null);
+  const [isBeginning, setIsBeginning] = useState(true);
+  const [isEnd, setIsEnd] = useState(false);
+
   return (
-    <Swiper
-      className={styles.root}
-      modules={[Grid]}
-      spaceBetween={16}
-      slidesPerView={2}
-      grid={{ fill: 'row', rows: 2 }}
-      breakpoints={{
-        768: {
-          spaceBetween: 36,
-          slidesPerView: 3,
-        },
-        1024: {
-          spaceBetween: 36,
-          slidesPerView: 4,
-        },
-      }}
-    >
-      {objects &&
-        objects.length > 0 &&
-        objects.map(item => (
-          <SwiperSlide key={item.title}>
-            <ObjectPreview object={item} isSmall={true} />
-          </SwiperSlide>
-        ))}
-    </Swiper>
+    <div className={styles.root}>
+      <Swiper
+        className={styles.swiper}
+        modules={[Grid, Pagination, Navigation]}
+        spaceBetween={16}
+        slidesPerView={2}
+        slidesPerGroup={2}
+        grid={{ fill: 'row', rows: 4 }}
+        pagination={{ type: 'fraction' }}
+        onSwiper={swiper => {
+          swiperRef.current = swiper;
+        }}
+        onSlideChange={swiper => {
+          setIsBeginning(swiper.isBeginning);
+          setIsEnd(swiper.isEnd);
+        }}
+        breakpoints={{
+          768: {
+            grid: { fill: 'row', rows: 3 },
+            spaceBetween: 36,
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+          },
+          1024: {
+            grid: { fill: 'row', rows: 3 },
+            spaceBetween: 36,
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+          },
+        }}
+      >
+        {objects &&
+          objects.length > 0 &&
+          objects.map(item => (
+            <SwiperSlide key={item.title}>
+              <ObjectPreview object={item} isSmall={true} />
+            </SwiperSlide>
+          ))}
+        <div className={styles.navigation}>
+          <Button
+            className={styles.prev}
+            onClick={() => swiperRef.current?.slidePrev()}
+            isTransparent={true}
+            disabled={isBeginning}
+          >
+            <ArrowCircleIcon />
+            Previous
+          </Button>
+          <Button
+            className={styles.next}
+            onClick={() => swiperRef.current?.slideNext()}
+            isTransparent={true}
+            disabled={isEnd}
+          >
+            Next
+            <ArrowCircleIcon />
+          </Button>
+        </div>
+      </Swiper>
+    </div>
   );
 };
 
